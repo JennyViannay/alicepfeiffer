03aa8d5222f9d2f64447f71b29b1c5a92ddde4ed
JennyViannay <jenny.viannay75@gmail.com>
2021-01-12 00:36:39 +0100

    fix home image bio & clean defaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,9 +16,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends AbstractController
 {
