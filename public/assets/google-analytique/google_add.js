8dd1ec7cd418d9b87fa1a554d4236c62661be82c
JennyViannay <jenny.viannay75@gmail.com>
2021-01-12 22:52:09 +0100

    google add

---
+++ public/assets/google-analytique/google_add.js
@@ -0,0 +1,18 @@
+console.log('[Google analytic HERE]')
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '{1002236826848591}',
+        cookie: true,
+        xfbml: true,
+        version: 'v9.0'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
